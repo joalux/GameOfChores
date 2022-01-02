5cea5238a6f095054c5c96d7a9e314a25b3eb9d0
joakim <joakim.lundb@gmail.com>
2022-01-02 17:01:51 +0100

    Initial Commit

---
+++ GamOfChores/GamOfChoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamOfChoresApp.swift
+//  GamOfChores
+//
+//  Created by joakim lundberg on 2022-01-02.
+//
+
+import SwiftUI
+
+@main
+struct GamOfChoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
